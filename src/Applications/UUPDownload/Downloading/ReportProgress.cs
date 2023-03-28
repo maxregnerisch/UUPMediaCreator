94a5342fb0bc3fb9b884fc58221a3b6ddb4b77fb
maxregnerisch <113422283+maxregnerisch@users.noreply.github.com>
2023-03-28 11:12:01 +0200

    Update ReportProgress.cs

--- src/Applications/UUPDownload/Downloading/ReportProgress.cs
+++ src/Applications/UUPDownload/Downloading/ReportProgress.cs
@@ -26,93 +26,28 @@ using System.Threading;
 
 namespace UUPDownload.Downloading
 {
-    public class ReportProgress : IProgress<GeneralDownloadProgress>, IDisposable
+    public class ConsoleProgress : IProgress<DownloadStatus>, IDisposable
+
     {
-        private readonly Dictionary<string, FileStatus> files = new();
-
-        private readonly Mutex mutex = new();
-
-        private static string FormatBytes(double bytes)
+        private readonly Mutex mutex = new Mutex();
+       
+        private static string FormatBytes(long bytes)
         {
-            string[] suffix = { "B", "KB", "MB", "GB", "TB" };
-            int i;
-            double dblSByte = bytes;
-            for (i = 0; i < suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            double len = bytes;
+            int order = 0;
+            while (len >= 1024 && order < sizes.Length - 1)
             {
-                dblSByte = bytes / 1024.0;
+                order++;
+                len = len / 1024;
             }
 
-            return $"{dblSByte:0.##}{suffix[i]}";
+            return $"{len:0.##} {sizes[order]}";
         }
 
-        private static string GetProgressBarString(int perc)
+        public void Report(DownloadStatus status)
         {
-            if (perc < 0)
-            {
-                perc = 0;
-            }
-
-            if (perc > 100)
-            {
-                perc = 100;
-            }
-
-            int eqsLength = (int)((double)perc / 100 * 55);
-            string bases = new string('=', eqsLength) + new string(' ', 55 - eqsLength);
-            bases = bases.Insert(28, perc + "%");
-            if (perc == 100)
-            {
-                bases = bases[1..];
-            }
-            else if (perc < 10)
-            {
-                bases = bases.Insert(28, " ");
-            }
-
-            return "[" + bases + "]";
-        }
-
-        public void Report(GeneralDownloadProgress e)
-        {
-            _ = mutex.WaitOne();
-
-            foreach (FileDownloadStatus status in e.DownloadedStatus)
-            {
-                if (status == null)
-                {
-                    continue;
-                }
-
-                bool shouldReport = !files.ContainsKey(status.File.FileName) || files[status.File.FileName] != status.FileStatus;
-
-                if (!shouldReport)
-                {
-                    continue;
-                }
-
-                files[status.File.FileName] = status.FileStatus;
-
-                string msg = "U";
-
-                switch (status.FileStatus)
-                {
-                    case FileStatus.Completed:
-                        msg = "C";
-                        break;
-                    case FileStatus.Downloading:
-                        msg = "D";
-                        break;
-                    case FileStatus.Expired:
-                        msg = "E";
-                        break;
-                    case FileStatus.Failed:
-                        msg = "F";
-                        break;
-                    case FileStatus.Verifying:
-                        msg = "V";
-                        break;
-                }
-
+            mutex.WaitOne();
                 Console.WriteLine($"{DateTime.Now:'['HH':'mm':'ss']'}[{e.NumFilesDownloadedSuccessfully}/{e.NumFiles}][E:{e.NumFilesDownloadedUnsuccessfully}][{msg}] {status.File.FileName} ({FormatBytes(status.File.FileSize)})");
             }
 
